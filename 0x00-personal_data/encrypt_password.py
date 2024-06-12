82dc23db7f4b049f2795c47ebbd6c52eaac94ab8
frankrazzih <francischege602@gmail.com>
2024-06-12 23:17:30 +0300

    user data

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -9,3 +9,8 @@ def hash_password(password: str)->bytes:
     salt = bcrypt.gensalt()
     hashed_pwd = bcrypt.hashpw(pwd_bytes, salt)
     return hashed_pwd
+
+def is_valid(hashed_password: bytes, password: str)->bool:
+    '''checks is a password is valid'''
+    hash_pwd = password.encode('UTF-8')
+    return bcrypt.checkpw(hash_pwd, hashed_password) 
