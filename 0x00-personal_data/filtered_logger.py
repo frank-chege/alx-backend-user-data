4a95af4debe9476cef843c3cae47f27f4224c836
frankrazzih <francischege602@gmail.com>
2024-06-12 17:26:35 +0300

    user data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -22,7 +22,8 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields)->None:
+    def __init__(self, fields: List[str])->None:
+        '''inits the format of the logs'''
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
