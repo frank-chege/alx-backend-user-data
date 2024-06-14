9ec04a3377efa425e18f7a09051276c8dd37334a
frankrazzih <francischege602@gmail.com>
2024-06-14 20:23:57 +0300

    basic authentication

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,7 +12,22 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+auth = getenv('AUTH_TYPE')
+if auth:
+    Auth = __import__('api.v1.auth.auth').Auth
+    auth = Auth()
 
+@app.before_request
+def before_request():
+    if auth is None:
+        pass
+    if auth.require_auth(request.path, ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']):
+        pass
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
 
 @app.errorhandler(404)
 def not_found(error) -> str:
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -18,7 +18,10 @@ class Auth:
     
     def authorization_header(self, request=None) -> str:
         '''auth header'''
-        return None
+        if request is None or ('Authorization' not in request.headers):
+            return None
+        else:
+            return request.headers.get('Authorization')
     
     def current_user(self, request=None) -> TypeVar('User'):
         '''current user'''
