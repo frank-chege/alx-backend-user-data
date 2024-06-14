47065aa5bd0c2d421dd04731e23fcaade7d013c3
frankrazzih <francischege602@gmail.com>
2024-06-14 19:48:19 +0300

    basic authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,7 +7,12 @@ class Auth:
     '''manages api authentication'''
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         '''require auth'''
-        return False
+        if path is None or excluded_paths is None:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        if path in excluded_paths:
+            return False
     
     def authorization_header(self, request=None) -> str:
         '''auth header'''
