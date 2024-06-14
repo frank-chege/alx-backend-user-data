8c975c3d0b83583fdbec1cceafa438d119af0f55
frankrazzih <francischege602@gmail.com>
2024-06-14 22:37:38 +0300

    basic authentication

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,7 +2,16 @@
 '''create basic auth class'''
 
 from api.v1.auth.auth import Auth
+import re
 
 class BasicAuth(Auth):
     '''basic auth'''
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        '''returns the base64 part of the auth header'''
+        if authorization_header is None or (not isinstance(authorization_header, str)):
+            return None
+        pat = re.compile('Basic(.*) $')
+        if not pat.match(authorization_header):
+            return None
+        tokens = authorization_header.split('Basic')
+        return tokens[1]
