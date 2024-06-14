4feab47ee40120868ad1b6bd8ba8b9790431179d
frankrazzih <francischege602@gmail.com>
2024-06-14 22:48:11 +0300

    basic authentication

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,8 +10,11 @@ class BasicAuth(Auth):
         '''returns the base64 part of the auth header'''
         if authorization_header is None or (not isinstance(authorization_header, str)):
             return None
-        pat = re.compile('Basic(.*)$')
+        pat = re.compile('Basic(.*)')
         if not pat.match(authorization_header):
             return None
         tokens = authorization_header.split('Basic')
         return tokens[1]
+test = BasicAuth()
+res = test.extract_base64_authorization_header('Basic1234')
+print(res)
