84e39d425d1d684ebfbac8fc98352c3610c55dba
frankrazzih <francischege602@gmail.com>
2024-06-16 00:15:46 +0300

    session authentication

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -11,8 +11,8 @@ class SessionAuth(Auth):
         '''creates a user's session id'''
         if user_id is None or (not isinstance(user_id, str)):
             return None
-        session_id = uuid.uuid4()
-        self.user_id_by_session_id[str(session_id)] = user_id
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
         return session_id
     
     def user_id_for_session_id(self, session_id: str = None) -> str:
