c6b3c55cf5e939230c4c0e0de25db9e4b30f5dd9
frankrazzih <francischege602@gmail.com>
2024-06-16 20:45:27 +0300

    user authentication service

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,7 +6,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import NoResultFound, InvalidRequestError
+from sqlalchemy.exc import NoResultFound, InvalidRequestError, SQLAlchemyError
 from typing import Any
 from user import Base, User
 
@@ -32,22 +32,27 @@ class DB:
             self.__session = DBSession()
         return self.__session
     
-    def add_user(self, email: str, hashed_password: str)->User:
-        '''adds a user's credentials to the db and returns the user instance'''
-        new_user = User(
-            email = email,
-            hashed_password = hashed_password
-        )
-        self._session.add(new_user)
-        self._session.commit()
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Adds a user's credentials to the db and returns the user instance"""
+        new_user = User(email=email, hashed_password=hashed_password)
+        try:
+            self._session.add(new_user)
+            self._session.commit()
+        except SQLAlchemyError as e:
+            self._session.rollback()
+            print(f"Error adding user: {e}")
+            raise
         return new_user
     
-    def find_user_by(self, **kwargs: Any)->Any:
-        '''fetch data based on the args passed'''
+    def find_user_by(self, **kwargs: Any) -> User:
+        """Fetches data based on the arguments passed"""
         try:
-            result = self.__session.query(User).filter_by(**kwargs).first()
+            result = self._session.query(User).filter_by(**kwargs).first()
             if result is None:
                 raise NoResultFound
-        except:
+        except NoResultFound:
+            raise
+        except SQLAlchemyError as e:
+            print(f"Error querying user: {e}")
             raise InvalidRequestError
         return result
