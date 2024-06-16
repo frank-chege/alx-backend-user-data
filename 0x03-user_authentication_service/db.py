e98760a6a8138043b5015e8c8be33af14035fe0c
frankrazzih <francischege602@gmail.com>
2024-06-16 19:53:01 +0300

    user authentication service

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,7 +6,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-
+from sqlalchemy.exc import NoResultFound, InvalidRequestError
+from typing import Any
 from user import Base, User
 
 
@@ -37,6 +38,11 @@ class DB:
             email = email,
             hashed_password = hashed_password
         )
-        user = self._session.add(new_user)
+        self._session.add(new_user)
         self._session.commit()
         return new_user
+    
+    def find_user_by(self, *kwargs: Any)->Any:
+        '''fetch data based on the args passed'''
+        result = self.__session.get(User).filter_by(kwargs).first()
+        return result
