0c7854348a677d9a6680d5997ab10103d521da48
frankrazzih <francischege602@gmail.com>
2024-06-16 18:11:18 +0300

    user authentication service

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -38,5 +38,5 @@ class DB:
             hashed_password = hashed_password
         )
         self._session.add(new_user)
-        user = self._session.commit()
-        return user
+        self._session.commit()
+        return new_user
