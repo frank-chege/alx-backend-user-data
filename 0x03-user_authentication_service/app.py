7b98905c100789fee034f54299e0e51a35b47585
frankrazzih <francischege602@gmail.com>
2024-06-17 15:09:48 +0300

    user authentication service

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -40,12 +40,14 @@ def sessions():
 def delete():
     '''deletes a users session_id'''
     try:
-        sessiod_id = request.cookies.get('session_id')
-        user = AUTH.get_user_from_session_id(sessiod_id)
-        AUTH.destroy_session(user.id)
-        return redirect('/'), 302
+        session_id = request.cookies.get('session_id')
+        user = AUTH.get_user_from_session_id(session_id)
     except:
         abort(403)
+    if user is None:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect('/'), 302
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
