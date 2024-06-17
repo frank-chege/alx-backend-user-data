001b877a2ee851ca2e501bc8157128cbf133f4a2
frankrazzih <francischege602@gmail.com>
2024-06-17 11:35:25 +0300

    user authentication service

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -44,6 +44,6 @@ class Auth:
             return False
     
     @property
-    def _generate_uuid(self):
+    def _generate_uuid(self)->str:
         '''creates a uuid'''
         return str(uuid.uuid4())
