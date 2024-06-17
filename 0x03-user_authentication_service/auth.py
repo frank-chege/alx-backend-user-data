7192f39786a9366f5ec9299601468ca9c0c72e82
frankrazzih <francischege602@gmail.com>
2024-06-17 13:13:46 +0300

    user authentication service

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -59,13 +59,15 @@ class Auth:
         except:
             return None
         
-    def get_user_from_session_id(self, session_id: str)->User:
-        '''gets a user based on their session id'''
+    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
+        """Retrieves a user based on a given session ID.
+        """
+        user = None
         if session_id is None:
             return None
         try:
             user = self._db.find_user_by(session_id=session_id)
-            return user
-        except:
-            return None     
+        except NoResultFound:
+            return None
+        return user
     
