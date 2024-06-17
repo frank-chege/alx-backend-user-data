55bd23c0ed32c000bc55c7894b9111fcfb414592
frankrazzih <francischege602@gmail.com>
2024-06-17 15:17:15 +0300

    user authentication service

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -46,5 +46,15 @@ def logout():
     AUTH.destroy_session(user.id)
     return redirect('/'), 302
 
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profile():
+    '''returns a users email'''
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if session_id is None or user is None:
+        abort(403)
+    else:
+        return jsonify({'email': user.email}), 200
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
